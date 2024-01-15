3fc2310824f1f1899522ba4728b2a364e3cbf074
Sathya <pasupatirajesh@gmail.com>
2024-01-15 13:34:22 -0600

    card display

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById('clickproject').addEventListener('click', function(){
+	var cardContent = document.getElementById('cardContent');
+	cardContent.style.displat = (cardContent.style.display === 'none') ? 'block' : 'none';
+
+});
