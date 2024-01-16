f430bffc59e2d9f3b47df9544707abae816785e2
Sathya <pasupatirajesh@gmail.com>
2024-01-16 14:42:36 -0600

    card to display detailed view

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-document.getElementById('clickproject').addEventListener('click', function(){
-	var cardContent = document.getElementById('cardContent');
-	cardContent.style.displat = (cardContent.style.display === 'none') ? 'block' : 'none';
+// document.getElementById('clickproject').addEventListener('click', function(){
+// 	var cardContent = document.getElementById('cardContent');
+// 	cardContent.style.display = (cardContent.style.display === 'none') ? 'block' : 'none';
 
-});
+// });
+
+function openModel() {
+	var model = document.getElementById('popupModel');
+	model.style.display = "flex";
+}
+
+function closeModel(){
+	var model = document.getElementById('popupModel');
+	model.style.display = "none";
+
+}
+
+document.getElementById('clickproject').addEventListener(
+	'click', function() {
+		openModel();
+	});
